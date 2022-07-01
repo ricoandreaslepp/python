077dbe9f44d38210f141510f05fc817ddef011b2
ricoandreaslepp <52963102+ricoandreaslepp@users.noreply.github.com>
2022-07-01 13:25:50 +0300

    Update solve.py

--- sudoku/solve.py
+++ sudoku/solve.py
@@ -1,28 +1,37 @@
 import requests as r
 import json
 
+class term_colors:
+	GREEN = '\033[92m'
+	RED = '\033[91m'
+	CYAN = '\033[96m'
+	ENDC = '\033[0m'
+
 class Sudoku:
 
 	URL = "https://sugoku.herokuapp.com"
 
-	def load_board_from_file(self, filename):
-		with open(filename) as f:
-			for li in f.readlines():
-				self.a.append(list(map(int, li.strip().split(" "))))
+	def __init__(self):
+		pass
 
-	# same api offers solution as well, for future reference
-	# https://github.com/bertoort/sugoku
-	def get_board_from_api(self, difficulty='easy'):
-		resp = r.get(f"{self.URL}/board?difficulty={difficulty}")
-		self.a = json.loads(resp.content.decode('UTF-8'))['board']
-	
 	# TO-DO
 	def check_solution(self):
 		return None
 
+	# TO-DO
+	def generate_puzzles(self):
+		return None
+
 	def print_board(self):
-		for x in self.a:
-			print(x)
+		for li in self.a:
+			for val in li:
+				if val==0:
+					print(f"{term_colors.RED}", end="")
+				else:
+					print(f"{term_colors.GREEN}", end="")
+
+				print(val, end=" ")
+			print()
 
 	def valid_row(self, row, col, val):
 		return True if val not in self.a[row] else False
@@ -49,12 +58,12 @@ class Sudoku:
 
 	def solve_sudoku(self):
 		self.print_board()
-		print("-"*((9*2)-1))
-		self._solve()
+		print(f"{term_colors.CYAN}" + "-"*((9*2)-1))
+		self.__solve()
 		self.print_board()
 
 	# Backtracking algorithm
-	def _solve(self):
+	def __solve(self):
 
 		if self.empty_square():
 			row, col = self.empty_square()
@@ -66,7 +75,7 @@ class Sudoku:
 			if self.valid(row, col ,i):
 				self.a[row][col] = i
 
-				if self._solve():
+				if self.__solve():
 					return True
 
 				# backtrack
@@ -74,6 +83,29 @@ class Sudoku:
 
 		return False
 
-s = Sudoku()
-s.get_board_from_api()
-s.solve_sudoku()
+	def load_board_from_file(self, filename):
+		try:
+			with open(filename) as f:
+				for li in f.readlines():
+					self.a.append(list(map(int, li.strip().split(" "))))
+		except FileNotFoundError:
+			print(f"No such file as {filename}...")
+
+	# same api offers solution as well, for future reference
+	# https://github.com/bertoort/sugoku
+	def get_board_from_api(self, difficulty='easy'):
+		resp = r.get(f"{self.URL}/board?difficulty={difficulty}")
+		self.a = json.loads(resp.content.decode('UTF-8'))['board']
+	
+
+if __name__=='__main__':
+
+	s = Sudoku()
+	s.get_board_from_api()
+	s.solve_sudoku()
+	"""
+	if action.strip().lower()=='file':
+		filename = input("Enter the name of the file to open: ")
+	elif action.strip().lower()=='api':
+		difficulty = input("Enter the difficulty to test: ")
+	"""
